e98095fedc2687090825fc2921dabc0cae8ae728
Emmanuel Nodolomwanyi <emma0martins@gmail.com>
2024-07-30 13:43:26 +0200

    Specified allowed origins

--- index.js
+++ index.js
@@ -6,9 +6,27 @@ const cors = require('cors');
 const app = express();
 const port = 3500;
 
+const allowedOrigins = ['http://localhost:3000', 'http://localhost:3500', 'https://static-site-test-77pb.onrender.com'];
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
-app.use(cors());
+app.use(cors(
+    {
+        origin: {
+            origin: allowedOrigins.indexOf('*') !== -1 ? '*' : function (origin, callback) {
+                if (!origin) return callback(null, true);
+                if (allowedOrigins.indexOf(origin) === -1) {
+                    var msg = 'The CORS policy for this site does not ' +
+                        'allow access from the specified Origin.';
+                    return callback(new Error(msg), false);
+                }
+                return callback(null, true);
+
+            },
+            credentials: true
+        }
+}
+));
 
 app.use(express.static('public'));
 
